eb9f7933a0e414e2b3ca02419a6c1a7fe897bfac
dpfurners <dpfurner@tsn.at>
2023-01-26 18:08:45 +0100

    Delete NamedClient, rename socket to conn in ClientBase

--- src/pygame_multiplayer/models/network.py
+++ src/pygame_multiplayer/models/network.py
@@ -4,14 +4,10 @@ import socket as _sock
 
 @_dc
 class ClientBase:
-    socket: _sock.socket
+    conn: _sock.socket
     address: str
     port: int
 
     @classmethod
-    def from_accept(cls, socket: _sock.socket, addr: tuple[str, int]):
-        return cls(socket, addr[0], addr[1])
-
-
-class NamedClient(ClientBase):
-    name: str
+    def from_accept(cls, conn: _sock.socket, addr: tuple[str, int]):
+        return cls(conn, addr[0], addr[1])
